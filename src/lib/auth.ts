9eaabec313c0fea98f453d5327acc6fb7115a8bd
dev-sajjadhosan <devsajjadhosan@gmail.com>
2026-01-28 18:55:19 +0600

    fix: lib/auth.ts file bugs

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,17 +3,11 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 import { config } from "../config";
 import { nodemailerTransporter } from "../helpers/nodemailer";
-import { after } from "node:test";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  plugins: {
-    after: {
-      
-    }
-  }
   user: {
     additionalFields: {
       role: {
